d48ef23220faeb4216310c5b0690224faac37d1d
gabrieljakubek <gabrieljakubek@gmail.com>
2019-06-18 00:23:08 -0300

    TP04

---
+++ Jakubek.Gabriel.2D.TP04/Entidades/GuardarString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Entidades
+{
+    public static class GuardarString
+    {
+        /// <summary>
+        /// Metodo para guardar un texto en un archivo
+        /// </summary>
+        /// <param name="texto">Lo que se desea guardar</param>
+        /// <param name="arcrivo">Nombre del archivo donde se guardara</param>
+        /// <returns></returns>
+        public static bool Guardar(this string texto, string arcrivo)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + arcrivo + ".txt"))
+                {
+                    sw.WriteLine(texto);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Jakubek.Gabriel.2D.TP04/FrmPpal/FrmPpal.cs
@@ -0,0 +1,93 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmPpal
+{
+    public partial class FrmPpal : Form
+    {
+        Correo correo;
+        public FrmPpal()
+        {
+            InitializeComponent();
+            correo = new Correo();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Paquete paquete = new Paquete(txtDireccion.Text, mtxtTrackingID.Text);
+            paquete.InformarEstado += new Paquete.DelegadoEstado(this.paq_InformaEstado);
+            try
+            {
+                this.correo += paquete;
+                ActualizarEstados();
+            }
+            catch(TrackingIdRepetidoException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnMostrarTodos_Click(object sender, EventArgs e)
+        {
+            this.MostrarInformacion<List<Paquete>>((IMostrar<List<Paquete>>)this.correo);
+        }
+
+        private void MostrarInformacion<T>(IMostrar<T> elemento)
+        {
+            if (!correo.Equals(null))
+            {
+                this.rtbMostrar.Text = elemento.MostrarDatos(elemento);
+                this.rtbMostrar.Text.Guardar("salida.txt");
+            }
+        }
+
+        private void ActualizarEstados()
+        {
+            lstEstadoIngresado.Items.Clear();
+            lstEstadoEnViaje.Items.Clear();
+            lstEstadoEntregado.Items.Clear();
+            foreach (Paquete paquete in correo.Paquetes)
+            {
+                switch (paquete.Estado)
+                {
+                    case Paquete.EEstado.Ingresado:
+                        lstEstadoIngresado.Items.Add(paquete);
+                        break;
+                    case Paquete.EEstado.EnViaje:
+                        lstEstadoEnViaje.Items.Add(paquete);
+                        break;
+                    case Paquete.EEstado.Entregado:
+                        lstEstadoEntregado.Items.Add(paquete);
+                        break;
+                }
+            }
+        }
+
+        private void paq_InformaEstado(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                Paquete.DelegadoEstado d = new Paquete.DelegadoEstado(paq_InformaEstado);
+                this.Invoke(d, new object[] { sender, e });
+            }
+            else
+            {
+                this.ActualizarEstados();
+            }
+        }
+        private void FrmPpal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            correo.FinEntregas();
+        }
+
+
+    }
+}
